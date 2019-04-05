a876d219f5040c5bf87b82448a5153419675e56a
nzawirski <myemail>
2019-04-05 16:53:52 +0200

    angular v2

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -4,19 +4,23 @@ import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {QuizComponent} from "./components/quiz/quiz.component";
 import {BlogComponent} from "./components/blog/blog.component";
-
+import {BlogItemDetailsComponent} from './components/blog-item-details/blog-item-details.component';
+import {BlogHomeComponent} from './blog-home/blog-home.component';
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
   },
   {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent
+  },    
+  {
     path: 'contact',
     component: ContactComponent,
   },
   {
-    path: 'blog',
-    component: BlogComponent,
+    path: 'blog', component: BlogHomeComponent,
 
   },
   {

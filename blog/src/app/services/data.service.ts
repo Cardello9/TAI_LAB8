f6ede5f3994bb0af180ca30b3e3fc51ed17f7e56
nzawirski <myemail>
2019-04-26 16:41:14 +0200

    lab8

--- blog/src/app/services/data.service.ts
+++ blog/src/app/services/data.service.ts
@@ -6,11 +6,12 @@ import {HttpClient} from '@angular/common/http';
 @Injectable()
 export class DataService {
 
-  private url = 'https://blogtai.herokuapp.com';
+  private url = 'http://localhost:8080';
 
 
   constructor(private http: HttpClient) { }
 
+  /*
   getAll() {
     return this.http.get(this.url + '/api/posts/');
     }
@@ -18,7 +19,18 @@ export class DataService {
     getById(id) {
       return this.http.get(this.url + '/api/posts/' + id);
 }
+*/
+
 getByText(data) {
 	return this.http.post(this.url + '/api/posts/', data);
 }
+
+
+  getAll() {
+    return this.http.get(`${this.url}/api/posts`);
+  }
+
+  getById(id) {
+    return this.http.get(`${this.url}/api/posts/${id}`);
+  }
 }
